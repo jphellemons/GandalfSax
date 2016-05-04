8fc34d3438fa914afe6d6d3b64379258385bc14c
JP Hellemons <info@jphellemons.nl>
2016-05-04 09:38:47 +0200

    dual screen support started

--- GandalfSax/App.xaml.cs
+++ GandalfSax/App.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows;
+using Application = System.Windows.Application;
 
 namespace GandalfSax
 {
@@ -13,5 +16,30 @@ namespace GandalfSax
     /// </summary>
     public partial class App : Application
     {
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0 || e.Args[0].ToLower().StartsWith("/s"))
+            {
+                foreach (Screen s in Screen.AllScreens)
+                {
+                    Window window = null;
+                    if (s.Primary)
+                        window = new MainWindow();
+                    else
+                        window = new MuteWindow();
+
+                    window.Left   = s.WorkingArea.Left;
+                    window.Top    = s.WorkingArea.Top;
+                    window.Show();
+                    window.WindowState = WindowState.Maximized;
+                }
+            }
+            else if (e.Args[0].ToLower().StartsWith("/p")) // preview screensaver
+            {
+            }
+            else if (e.Args[0].ToLower().StartsWith("/c")) // configure screensaver
+            {
+            }
+        }
     }
 }
--- GandalfSax/MuteWindow.xaml.cs
+++ GandalfSax/MuteWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GandalfSax
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GifWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MuteWindow : Window
     {
-        public MainWindow()
+        public MuteWindow()
         {
             InitializeComponent();
-            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);        
-            registryKey.SetValue("GandalfSax", System.Reflection.Assembly.GetExecutingAssembly().Location);
         }
 
         private void me_MediaEnded(object sender, RoutedEventArgs e)
